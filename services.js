b5e763bb803165d721b75e94846ae75b7289c466
prakhil-tp <prakhil.tp@gmail.com>
2020-12-17 18:03:18 +0530

    feat: Create tables and insert data

--- services.js
+++ services.js
@@ -2,6 +2,7 @@ const fs = require("fs");
 const puppeteer = require("puppeteer");
 const cheerio = require("cheerio");
 const cliProgress = require("cli-progress");
+const knex = require("./db");
 const Lazada = require("./lazada");
 const Shopee = require("./shopee");
 
@@ -28,6 +29,91 @@ function File(path, platform) {
     }
   };
 
+  const createDbTable = async () => {
+    const isTableExist = await knex.schema.hasTable(platform);
+    if (!isTableExist) {
+      switch (platform) {
+        case "lazada":
+          await knex.schema.createTable("lazada", (table) => {
+            table.increments("_id").primary();
+            table.string("date", 50);
+            table.string("link", 300);
+            table.string("title", 500);
+            table.string("category", 100);
+            table.string("brand", 100);
+            table.string("price", 50);
+            table.string("discount", 500);
+            table.string("promotions", 500);
+            table.string("seller", 200);
+            table.string("positive_seller_rating", 50);
+            table.string("ship_on_time", 50);
+            table.string("chat_response_rate", 50);
+            table.string("authenticity", 100);
+            table.string("warranty_type", 300);
+            table.string("warranty_period", 100);
+            table.string("standard_delivery_time", 100);
+            table.string("shipping_cost", 100);
+            table.string("rating");
+            table.string("five_star_count");
+            table.string("four_star_count");
+            table.string("three_star_count");
+            table.string("two_star_count");
+            table.string("one_star_count");
+            table.boolean("availability");
+          });
+          break;
+        case "shopee":
+          await knex.schema.createTable("shopee", (table) => {
+            table.increments("_id").primary();
+            table.string("date", 50);
+            table.string("link", 300);
+            table.string("title", 500);
+            table.string("category", 300);
+            table.string("brand", 150);
+            table.string("shop", 100);
+            table.string("best_before", 50);
+            table.string("stock_available");
+            table.string("price", 100);
+            table.string("discount", 100);
+            table.string("promo");
+            table.string("bundle_deal_recommendation");
+            table.string("rating", 50);
+            table.string("num_of_ratings", 100);
+            table.string("sold", 100);
+            table.string("free_shipping_with_order_of_xx", 100);
+            table.string("shipping_cost", 100);
+            table.string("shop_ratings", 100);
+            table.string("products_count", 100);
+            table.string("response_rate", 50);
+            table.string("response_time", 100);
+            table.string("followers", 100);
+            table.string("shop_voucher");
+          });
+          break;
+      }
+    }
+  };
+
+  const insertToDB = async (data) => {
+    try {
+      const date = getDate();
+      const row = await knex
+        .select("*")
+        .from(platform)
+        .where("link", data.link);
+      if (row && row.length) {
+        knex(platform)
+          .where({ _id: row[0]._id })
+          .update({ ...data });
+      } else {
+        await knex(platform).insert({ date, ...data });
+      }
+    } catch (e) {
+      fs.writeFileSync("error.log", e.message);
+      fs.appendFileSync("error.log", JSON.stringify(e));
+    }
+  };
+
   const appendToFile = (data) => {
     if (!data) {
       return fs.appendFileSync(path, `\n ${++counter},N.A,,,,,,,,,,,,,,,`);
@@ -38,10 +124,12 @@ function File(path, platform) {
       fileText += `"${value}",`;
     });
 
+    insertToDB(data);
     fs.appendFileSync(path, fileText, { encoding: "utf8" });
   };
 
   createFile();
+  createDbTable();
   return { appendToFile };
 }
 
@@ -67,15 +155,15 @@ async function getHtml(url, platform) {
       if (isCaptcha) throw new Error("Captcha detected");
     } catch (e) {
       if (browser) await browser.close();
-      console.log("\n Captcha detected!, will retry after 1 minute");
-      await new Promise((resolve) => setTimeout(resolve, 60 * 1000));
+      console.log("\n Captcha detected!, will retry after 2 minute");
+      await new Promise((resolve) => setTimeout(resolve, 120 * 1000));
       return getHtml(url, platform);
     }
   } else {
     try {
-      await page.goto(url, { waitUntil: "networkidle0", timeout: 30000 });
+      await page.goto(url, { waitUntil: "networkidle0" });
     } catch (e) {
-      console.log("\n ", e.message);
+      //console.log("\n ", e.message);
     }
     html = await page.content();
   }
